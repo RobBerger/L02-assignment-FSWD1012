45f07aec5273b6701432b2bb77ef5db5d429aed9
berger <robertberger.dev@gmail.com>
2022-12-15 20:59:58 -0500

    Created a loop that iterates through the words array

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function arrayFilter(sentence){
     var words = sentence.split(" ");
     var biggerThanThree = [];
+
+    for (var i = 0; i < words.length; i++) {
+        console.log(words[i]);
+    }
 }
 
 arrayFilter("London is a big city in the United Kingdom");
